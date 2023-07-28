898f1d519ee8543366d80bf978ba626c5dd774af
tadrius <116924580+tadrius@users.noreply.github.com>
2023-07-28 16:37:06 -0400

    Build Project

--- Assets/Scripts/CharacterHealth.cs
+++ Assets/Scripts/CharacterHealth.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.WSA;
 
 public class CharacterHealth : MonoBehaviour
 {
@@ -48,7 +46,7 @@ public class CharacterHealth : MonoBehaviour
 
     void ShowDamageDisplay()
     {
-        if (currentDamageDisplayIndex >= damageDisplays.Count())
+        if (currentDamageDisplayIndex >= damageDisplays.Length)
         {
             currentDamageDisplayIndex = 0;
         }
